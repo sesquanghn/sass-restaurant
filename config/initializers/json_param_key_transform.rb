5ca30aeb6fecd222799099eeb7087af95371fc58
quanghn <hn.quang@anysense.co.jp>
2022-02-25 10:01:56 +0700

    feat(Customer): camelize params

--- config/initializers/json_param_key_transform.rb
+++
@@ -1,6 +0,0 @@
-ActionDispatch::Request.parameter_parsers[:json] = ->(raw_json) {
-  data = ActiveSupport::JSON.decode(raw_json)
-  data = { _json: data } unless data.is_a?(Hash)
-
-  data.deep_transform_keys!(&:underscore)
-}
