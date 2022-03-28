c911a848b3908eb862a23a7f57871af499918421
quanghn <hn.quang@anysense.co.jp>
2022-03-28 15:53:32 +0700

    feat(Reservation): add relationships to response

--- app/queries/reservation_queries.rb
+++ app/queries/reservation_queries.rb
@@ -1,7 +1,7 @@
 class ReservationQueries < BaseQueries
   def by_reserved_at(date = nil)
     date ||= Date.current
-    @result = @result.where("reservations.start_time::date = ? AND reservations.end_time::date = ?", date, date)
+    @result = @result.where("reservations.start_time::date = :q AND reservations.end_time::date = :q", q: date)
     self
   end
 end
